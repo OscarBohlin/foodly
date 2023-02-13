4986ae38ff5d8596136ca19e1aa80ab882c9ae0e
Oscar Bohlin <c20obn@cs.umu.se>
2023-02-13 17:09:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+## Notes
+
+css: https://github.com/CoreyMSchafer/code_snippets/tree/master/Python/Flask_Blog
---
+++ forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import InputRequired 
+
+
+class BarOrderForm(FlaskForm):
+
+	handled_by = StringField("Namn", validators=[InputRequired()])
+	submit = SubmitField("Lägg beställning")
